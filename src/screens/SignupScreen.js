0f45fad14a0224e68879cfce505e64a587104cfb
Petar Petkov <pgpetkov@protonmail.com>
2020-11-22 14:20:24 +0200

    Add signup forms from react-elements, and wire initial state

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-  return (
-    <>
-      <Text style={{ fontSize:48 }}>
-        SignupScreen
-      </Text>
-      <Button title="Signin" onPress={() => navigation.navigate('Signin')} />
-      <Button title="Main Flow" onPress={() => navigation.navigate('mainFlow')} />
-    </>
-  );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3 style={styles.header}>Sign Up for Tracker!</Text>
+        <Input 
+          label="Email"  
+          value={email} 
+          onChangeText={(newEmail) => setEmail(newEmail)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Input
+          secureTextEntry={true}
+          label="Password"  
+          value={password} 
+          onChangeText={(newPassword) => setPassword(newPassword)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
 };
 
 const styles = StyleSheet.create({
-
-})
+  container: {
+    // Fill vertical space
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 100
+  },
+  header: {
+    paddingBottom: 50,
+    marginLeft: 55
+  }
+});
 
 export default SignupScreen;
