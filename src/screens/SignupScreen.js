9abb4966c198cdc11c51c5f0d19092423d9a1840
Petar Petkov <pgpetkov@protonmail.com>
2020-11-22 13:22:50 +0200

    Add boilerplate screens

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SignupScreen = () => {
+  return (
+    <Text style={{ fontSize:48 }}>
+      SignupScreen
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default SignupScreen;
---
+++ src/screens/TrackDetailScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TrackDetailScreen = () => {
+  return (
+    <Text style={{ fontSize:48 }}>
+      TrackDetailScreen
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default TrackDetailScreen;
