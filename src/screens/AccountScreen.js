220e90645106ab12512f930e872c22052faecb99
Petar Petkov <pgpetkov@protonmail.com>
2020-11-24 07:41:47 +0200

    Add some styling to sigout screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
 
@@ -8,20 +9,25 @@ const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
 
   return (
-    <>
-    <Spacer>
-      <Text style={{ fontSize:48 }}>AccountScreen</Text>
-      <Button 
-        title="Sing Out"
-        onPress={() => signout()}
-      />
-    </Spacer>
-    </>
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Spacer>
+        <Text style={styles.header}>AccountScreen</Text>
+        <Button 
+          title="Sign Out"
+          onPress={() => signout()}
+        />
+      </Spacer>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  header: {
+    fontSize: 48,
+    paddingBottom: 15,
+    textAlignVertical: "center",
+    textAlign: "center"
+  },
 })
 
 export default AccountScreen;
