be0d8edf126045086fe44c7041b8468bd6c396c6
Petar Petkov <pgpetkov@protonmail.com>
2020-11-24 07:08:06 +0200

    Add navigation listener to clear error messages between screens

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink';
 
-const SigninScreen = ({ navigation }) => {
-  const { state, signin } = useContext(AuthContext) 
+const SigninScreen = () => {
+  const { state, signin, clearErrorMessage } = useContext(AuthContext) 
 
   return (
     <View style={styles.container}>
+        <NavigationEvents 
+          onWillFocus={() => {clearErrorMessage()}}
+        />
         <AuthForm 
           headerText="Sign In"
           errorMessage={state.errorMessage}
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
 import AuthForm from '../components/AuthForm'
 import NavLink from '../components/NavLink';
 
-const SignupScreen = ({ navigation }) => {
-
-  const { state, signup } = useContext(AuthContext) 
-
+const SignupScreen = () => {
+  const { state, signup, clearErrorMessage } = useContext(AuthContext) 
   return (
     <View style={styles.container}>
+        <NavigationEvents 
+          onWillFocus={() => {clearErrorMessage()}}
+        />
         <AuthForm 
           headerText="Sign up for Tracker"
           errorMessage={state.errorMessage}
