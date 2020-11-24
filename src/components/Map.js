b36526ea2566dfd98d64eb2c35fd24543262a20c
Petar Petkov <pgpetkov@protonmail.com>
2020-11-24 07:47:31 +0200

    Add map boilerplate and styling to track create screen

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Map = () => {
+  return (
+    <Text>I AM MAP</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { Text } from 'react-native-elements';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
   return (
-    <Text style={{ fontSize:48 }}>
-      TrackCreateScreen
-    </Text>
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h3 style={styles.header}>TrackCreateScreen</Text>
+      <Map />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  header: {
+    paddingBottom: 15,
+    textAlignVertical: "center",
+    textAlign: "center"
+  },
 })
 
 export default TrackCreateScreen;
