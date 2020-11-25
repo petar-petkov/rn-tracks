ec28328c311d35edfa3726c7bf47da943425b6e0
Petar Petkov <pgpetkov@protonmail.com>
2020-11-25 06:32:15 +0200

    Move location logic into reusable hook

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,9 +4,8 @@ import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-  const test = false;
   const { state: { currentLocation } } = useContext(LocationContext);
-  
+
   if (!currentLocation) {
     return <ActivityIndicator size='large' style={{ marginTop: 200 }} />;
   }
