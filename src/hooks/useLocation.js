ce20a2cccde6372bc782783d7847b7a51ccaabf5
Petar Petkov <pgpetkov@protonmail.com>
2020-11-25 06:47:53 +0200

    Add function to stop tracking location if user is no longer on map screen

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -5,8 +5,9 @@ import {
   watchPositionAsync,
 } from 'expo-location';
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
 
   const startWatching = async () => {
     try {
@@ -14,7 +15,10 @@ export default (callback) => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
-      await watchPositionAsync(
+      // The only way to stop tracking is to remove the watch
+      // position function i.e. sub.remove()
+      // this is best achieved with using state
+      const sub = await watchPositionAsync(
         {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
@@ -22,14 +26,28 @@ export default (callback) => {
         },
         callback
       );
+      // Update the state with our current watcher/subscriber
+      setSubscriber(sub);
     } catch (e) {
       setErr(e);
     }
   };
 
   useEffect(() => {
-    startWatching();
-  }, []);
+    // Check what the value of should track is or more accurately if 
+    // our screen with the map is currently focused, if it is
+    // we track location and if not we remove the background
+    // process for watching
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      // Delete the subscriber
+      subscriber.remove();
+      // Also reset state
+      setSubscriber(null);
+    }
+  // We want to re-render everything should track value changes
+  }, [shouldTrack]);
 
   return [err];
 };
