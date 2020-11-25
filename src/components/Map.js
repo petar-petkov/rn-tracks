a2944efde1cdbdce23c1ae439335d5265a6e5369
Petar Petkov <pgpetkov@protonmail.com>
2020-11-25 06:12:38 +0200

    Add circle to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text, StyleSheet, ActivityIndicator } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -25,6 +25,12 @@ const Map = () => {
         longitudeDelta: 0.01
       }}
     >
+      <Circle 
+        center={currentLocation.coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
     </MapView>
   );
 };
