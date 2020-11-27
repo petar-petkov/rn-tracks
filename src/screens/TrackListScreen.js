d2dc5c3c551ca1be3c1fbbb75e85a1621ef95e77
Petar Petkov <pgpetkov@protonmail.com>
2020-11-27 07:46:27 +0200

    Create FlatList with all of our tracks and bit of style

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements'
+import { Context as TrackContext } from '../context/TrackContext';
+
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+  
   return (
     <>
-      <Text style={{ fontSize:48 }}>
-        TrackListScreen
-      </Text>
-      <Button 
-        title="Track Detail" 
-        onPress={() => navigation.navigate('TrackDetail')} 
+      <NavigationEvents onWillFocus={() => fetchTracks()} />
+      <Text style={{ fontSize:48 }}>TrackListScreen</Text>
+      <FlatList
+        data={state}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ListItem chevron={true} title={item.trackName} />
+            </TouchableOpacity>
+          )
+        }}
       />
     </>
   );
